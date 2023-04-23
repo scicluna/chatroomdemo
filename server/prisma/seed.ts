43aabe21e1bfce8cf67ef3414d149e45e27e79b0
scicluna <88948700+scicluna@users.noreply.github.com>
2023-04-23 16:51:36 -0400

    prisma outline kind of working

---
+++ server/prisma/seed.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    // Create a user with a chat
+    const newUser = await prisma.user.create({
+        data: {
+            email: 'test@example.com',
+            username: 'testuser',
+            chats: {
+                create: {
+                    title: 'My First Chat',
+                    body: 'This is the first chat I created.',
+                },
+            },
+        },
+        include: {
+            chats: true,
+        },
+    });
+
+    console.log('Created new user with a chat:', newUser);
+}
+
+main()
+    .catch((error) => {
+        console.error(error);
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
