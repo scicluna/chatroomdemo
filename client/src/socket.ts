97f23a92ee44302811a2637375a542764c125c51
scicluna <88948700+scicluna@users.noreply.github.com>
2023-04-25 12:07:05 -0400

    trying something new

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
-const isProduction = process.env.NODE_ENV === "production";
-const socketURL = isProduction
-    ? "wss://voidchat.herokuapp.com"
-    : "ws://localhost:3001";
+// const isProduction = process.env.NODE_ENV === "production";
+// // const socketURL = isProduction
+// //     ? "wss://voidchat.herokuapp.com"
+// //     : "ws://localhost:3001";
 
-const socket = io(socketURL);
+const socket = io();
 
 export default socket;
